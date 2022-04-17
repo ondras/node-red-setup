bd251addb83c32358e8361951868f571957a5043
Ondřej Žára <ondrej.zara@gmail.com>
2022-04-17 07:46:51 +0000

    no bt, rtl renaming, cleanup & update

--- gtfs/update.sh
+++ gtfs/update.sh
@@ -4,9 +4,16 @@ cd $(dirname $0)
 FILE=PID_GTFS.zip
 DIR=static
 
+logger -t gtfs starting and downloading...
 wget http://data.pid.cz/$FILE
+
+logger -t gtfs unzipping...
 mkdir -p $DIR
 unzip -o $FILE -d $DIR
 rm $FILE
 
+logger -t gtfs db updating...
 sqlite3 gtfs.sqlite ".read gtfs_import.sql"
+
+logger -t gtfs done
+
